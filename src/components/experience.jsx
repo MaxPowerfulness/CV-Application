bb013d6165bf98d875775a9f26040889dba74f08
Sean Gombart <skgombart@gmail.com>
2023-08-26 14:55:15 -0700

    Added work experience component

---
+++ src/components/experience.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import '../styles/experience.css'
+
+function Experience() {
+    
+    return (
+        <section id="section">
+            <div className="jobHeader">
+                <h1>Experience</h1>
+                <button className="addJob">+</button>
+            </div>
+            <Job />
+        </section>
+    )
+}
+
+function Job() {
+    const [title, setTitle] = useState('')
+    const [company, setCompany] = useState('')
+    return (
+        <div className="job">
+            <p><input placeholder="Title" value={title} onChange={(e) => setTitle(e.target.value)} /> | <input type="date" name="startDate"/> to <input type="date" name="endDate"/></p>
+            <input placeholder="Company" name="company" value={company} onChange={(e) => setCompany(e.target.value)} /> 
+            <ul className="workExperience">
+                <li><WorkExperience /></li>
+                <li><WorkExperience /></li>
+                <li><WorkExperience /></li>
+            </ul>
+        </div>
+    )
+}
+
+function WorkExperience() {
+    const [experience, setExperience] = useState('')
+
+    return (
+        <>
+            <input placeholder="Work Experience" value={experience} onChange={(e) => setExperience(e.target.value)}  />
+        </>
+    )
+}
+
+export default Experience
