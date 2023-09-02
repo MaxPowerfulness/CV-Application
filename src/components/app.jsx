785f8bb03c7171059d2d6ab1d6bcb7323e64ed39
Sean Gombart <skgombart@gmail.com>
2023-09-01 17:14:22 -0700

    fixed experience form submit and update logic

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -16,7 +16,7 @@ function App() {
     const [location, setLocation] = useState('');
     const [website, setWebsite] = useState('');
     const [summary, setSummary] = useState('');
-    const [experience, setExperience] = useState(null)
+    const [experience, setExperience] = useState([])
     // define setState functions here
     function onName(e) {
         setFullName(e.target.value)
@@ -46,23 +46,52 @@ function App() {
         setSummary(e.target.value)
     }
 
-    function onExperience(e) {
+    function onExperienceSubmit(e) {
         e.preventDefault();
-        console.log(e.target)
-        const experience = {};
-        experience.id = v4();
-        experience.title = e.target[0].value
+        const experienceData = {};
+        experienceData.id = v4();
+        experienceData.title = e.target[0].value;
+        experienceData.company = e.target[1].value;
+        experienceData.start = e.target[2].value;
+        experienceData.end = e.target[3].value;
+        experienceData.description = e.target[4].value;
+        setExperience([...experience, experienceData]);
+        clearForm(e)
     }
+
+    function updateForm(e, id) {
+        e.preventDefault();
+        setExperience(
+            experience.map(job => {
+                if (job.id === id) {
+                    const updatedExperience = {}
+                    updatedExperience.id = id;
+                    updatedExperience.title = e.target[0].value;
+                    updatedExperience.company = e.target[1].value;
+                    updatedExperience.start = e.target[2].value;
+                    updatedExperience.end = e.target[3].value;
+                    updatedExperience.description = e.target[4].value;
+                    return updatedExperience
+                }
+            })
+        )
+        console.log('experience', experience)
+    }
+
+    function clearForm(e) {
+        for (let i = 0; i < 5; i++) e.target[i].value = ''
+    }
+
 
     return (
         <main>
-            <section id='enterInfo'>
+            <section id='enterInfo'>    
                 <GerneralInfo onName={onName} onPosition={onPosition} onPhoneNumber={onPhoneNumber} onEmail={onEmail} onLocation={onLocation} onWebsite={onWebsite} />
                 <Summary onSummary={onSummary} /> 
-                <Experience onSubmit={onExperience} />
+                <Experience onExperienceSubmit={onExperienceSubmit} updateForm={updateForm} experiences={experience} />
             </section>
             <section id='preview'>
-                <Preview fullName={fullName} position={position} phoneNum={phoneNum} email={email} location={location} website={website} summary={summary} />
+                <Preview fullName={fullName} position={position} phoneNum={phoneNum} email={email} location={location} website={website} summary={summary} experience={experience} />
             </section>
         </main>
         // nav bar to adjust settings/print
--- src/components/form/experience.jsx
+++ src/components/form/experience.jsx
@@ -1,30 +1,51 @@
 import '../../styles/experience.css'
 
-function Experience({ onSubmit }) {
+
+function Experience({ onExperienceSubmit, updateForm, experiences }) {
+    const experienceForms = experiences.map(experience => {
+        return(
+        <ExperienceForm
+            key={experience.id}
+            onExperienceSubmit={(e) => updateForm(e, experience.id)}
+            title={experience.title} company={experience.company}
+            start={experience.start}
+            end={experience.end}
+            description={experience.description} />)
+    })
+    
     return (
-        <form action="" onSubmit={onSubmit}>
+        <section>
             <h1>Experience</h1>
+            <ExperienceForm onExperienceSubmit={onExperienceSubmit} />
+            {experienceForms}
+        </section>
+    )
+}
+
+function ExperienceForm({ onExperienceSubmit, title, company, start, end, description }) {
+    return (
+        <form action="" onSubmit={onExperienceSubmit}>
             <p className="formInput">
                 <label htmlFor="title">Title: </label>
-                <input type="text" id="title" />
+                <input type="text" id="title" defaultValue={title} />
             </p>
             <p className="formInput">
                 <label htmlFor="company">Company: </label>
-                <input type="text" id="company" />
+                <input type="text" id="company" defaultValue={company} />
             </p>
             <p className="formInput">
                 <label htmlFor="start">Start: </label>
-                <input type="date" id="start" />
+                <input type="date" id="start" defaultValue={start} />
                 <label htmlFor="end">End: </label>
-                <input type="date" id="end" />
+                <input type="date" id="end" defaultValue={end} />
             </p>
             <p className="formInput">
                 <label htmlFor="duties">Description: </label>
-                <textarea type="text" id="description"></textarea>
+                <textarea type="text" id="description" defaultValue={description}></textarea>
             </p>
             <div className="btnCont">
                 <button>Add</button>
-                <button type="button">Remove</button>
+                <button type="button" className='removeExperience'>Remove</button>
             </div>
         </form>
     )
