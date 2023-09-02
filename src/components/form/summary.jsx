40d77d221dcf10d3c66941cba5369ce9de2afd35
Sean Gombart <skgombart@gmail.com>
2023-09-02 10:26:47 -0700

    removed label

--- src/components/form/summary.jsx
+++ src/components/form/summary.jsx
@@ -5,7 +5,6 @@ function Summary({onSummary}) {
     return (
         <form>
             <h1>Summary</h1>
-            <label htmlFor="summaryText">Summary</label>
             <textarea name="summary" id="summaryText" cols="55" rows="4" maxLength="280" placeholder="Tell me about yourself" onChange={onSummary}></textarea>
         </form>
     )
