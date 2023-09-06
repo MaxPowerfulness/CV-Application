f07c79fc5b28078171c556246f5a8b716dd73491
Sean Gombart <skgombart@gmail.com>
2023-09-05 17:23:28 -0700

    Added skills

--- src/components/preview/preview.jsx
+++ src/components/preview/preview.jsx
@@ -2,15 +2,17 @@ import GerneralInfoPreview from './general-info-preview'
 import SummaryPreview from './summary-preview'
 import ExperiencePreview from './experience-preview'
 import EducationPreview from './education-preview'
+import SkillsPreview from './skills-preview'
 import '../../styles/preview.css'
 
-function Preview({fullName, position, phoneNum, email, location, website, summary, experience, education}) {
+function Preview({fullName, position, phoneNum, email, location, website, summary, experience, education, skills}) {
     return (
         <div>
             <GerneralInfoPreview fullName={fullName} position={position} phoneNum={phoneNum} email={email} location={location} website={website} />
             <SummaryPreview summary={summary} />
             <ExperiencePreview experience={experience} />
             <EducationPreview education={education} />
+            <SkillsPreview skills={skills} />
         </div>
 
     )
