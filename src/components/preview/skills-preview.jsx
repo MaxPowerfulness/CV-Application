482e4ec63fe17a4b5fb189222ebbf88da85d4359
Sean Gombart <skgombart@gmail.com>
2023-09-08 16:47:10 -0700

    Adds new list for every 4 skills

--- src/components/preview/skills-preview.jsx
+++ src/components/preview/skills-preview.jsx
@@ -1,12 +1,32 @@
-function SkillsPreview({skills}) {
-    const skillsList = skills.map(skill => <li key={skill.id}>{skill.skill}</li>)
+import { createElement } from "react";
+import {v4} from 'uuid'
+
+function SkillsPreview({ skills }) {
+    let skillList = []
+    let regList = []
+    let newList = null
+
+    skills.map((skill, index) => {
+        if (index === 0) {
+            newList = createElement('ul', { key: v4()}, regList)
+            regList.push(<li key={skill.id}>{skill.skill}</li>)
+            skillList.push(newList)
+        } else if (index % 4 === 0 && index !== 0) {
+            regList = []
+            regList.push(<li key={skill.id}>{skill.skill}</li>)
+            newList = createElement('ul', { key: v4() }, regList);
+            skillList.push(newList)
+        } else {
+            regList.push(<li key={skill.id}>{skill.skill}</li>)
+        }
+    })
 
     return (
         <section>
             <h1>Skills</h1>
-            <ul className="skillList">
-                {skillsList}
-            </ul>
+            <div>
+                {skillList}
+            </div>
         </section>
     )
 }
