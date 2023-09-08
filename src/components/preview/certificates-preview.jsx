52312ea9aee6a45338c0b5ce793af5e5ef21cdcc
Sean Gombart <skgombart@gmail.com>
2023-09-08 16:59:39 -0700

    New list every 4 certs

--- src/components/preview/certificates-preview.jsx
+++ src/components/preview/certificates-preview.jsx
@@ -1,12 +1,32 @@
+import { createElement } from "react";
+import {v4} from 'uuid'
+
 function CertificatePreview({ certificates }) {
-    const certList = certificates.map(cert => <li key={cert.id}>{cert.cert}</li>)
+    let certList = []
+    let tempList = []
+    let newList = null
+
+    certificates.map((cert, index) => {
+        if (index === 0) {
+            newList = createElement('ul', { key: v4()}, tempList)
+            tempList.push(<li key={cert.id}>{cert.cert}</li>)
+            certList.push(newList)
+        } else if (index % 4 === 0 && index !== 0) {
+            tempList = []
+            tempList.push(<li key={cert.id}>{cert.cert}</li>)
+            newList = createElement('ul', { key: v4() }, tempList);
+            certList.push(newList)
+        } else {
+            tempList.push(<li key={cert.id}>{cert.cert}</li>)
+        }
+    })
    
     return (
         <section>
             <h1>Certificates</h1>
-            <ul className="certificateList">
+            <div className="previewContent">
                 {certList}
-            </ul>
+            </div>
         </section>
     )
 }
